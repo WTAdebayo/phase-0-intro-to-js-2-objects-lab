8765b2b27e076a63461e1c624d1fb3611bd2d945
Adewole Adebayo <wole1105@gmail.com>
2022-05-27 02:59:42 -0700

    commit tree 2ceafa03b760380fc1c60e0c9766f21e92273eae

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+    name : 'George',
+    streetAddress : '8th Street NW',
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function updateEmployeeWithKeyAndValue(employee, streetAddress, ) {
+      const newEmployee = {...employee}
+
+     newEmployee[streetAddress] = '11 Broadway';
+     
+     return newEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetName, ) {
+    employee[streetName] = '12 Broadway';
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, name) {
+      const newEmployee = {...employee}
+
+      delete newEmployee.name
+
+      return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, name) {
+      delete employee.name
+        return employee;
+  }
